70fe6dc9a5ec1c7af8fc86fcaacf792e4e553196
dprabin <dprabin@yahoo.com>
2015-06-02 09:29:23 +0545

    Changed some functions to private and cleaned comments

--- application/models/Tfidf_cosine_model.php
+++ application/models/Tfidf_cosine_model.php
@@ -1,24 +1,25 @@
 <?php
 class Tfidf_cosine_model extends CI_Model{
 
-	//This model needs a table named allwords
-	//$tablename = 'allwords';
-	//CREATE TABLE allwords (doc_id int(11) NOT NULL, word varchar(50) NOT NULL, count int(11) NOT NULL, `keyword` tinyint(1) NOT NULL DEFAULT '0')
-	//Other table requirements are keywords(id, keywords), docs_keywords_link (id, doc_id,keyword_id)
+	/*
+	 * This model needs a table named allwords
+	 * $tablename = 'allwords';
+	 * CREATE TABLE allwords (doc_id int(11) NOT NULL, word varchar(50) NOT NULL, count int(11) NOT NULL, `keyword` tinyint(1) NOT NULL DEFAULT '0')
+	 */
 
-	//words array to use in different functions
-	//$words = array();
-	//total document count
+
 	private $alldoc_count = 5000;
 
 	function __construct() {
 		parent::__construct();
 		$alldoc_count = $this->get_no_of_documents();
-		//error_log("all doc count = $alldoc_count",0);
 	}
 
-	//Count total number of documents in the system
-	public function get_no_of_documents(){
+	/* 
+	 * Count total number of documents in the system
+	 * @return int total_no_of_docs
+	 */
+	private function get_no_of_documents(){
 		$this->db->select('count(distinct doc_id) as nodocs');
 		$this->db->from('allwords');
 		$query = $this->db->get()->row();
@@ -27,22 +28,30 @@ class Tfidf_cosine_model extends CI_Model{
 
 	//////////////////////////////////////// functions to clean and count text to words
 
-	//Simple stemmer function to remove words less than 2 characters
-	//Convert string to lower case
-	//don't bother inserting small words like a, an, on, of, to, by etc.
-	//they don't carry any meaning to the text and don't make keywords
-	//TOCHANGE: use lemmatizer algorithm instead of simple stemmer
-	public function simple_stemmer($string){
+	/*
+	 * Simple stemmer function to remove words less than 2 characters
+	 * Convert string to lower case
+	 * don't bother inserting small words like a, an, on, of, to, by etc.
+	 * they don't carry any meaning to the text and don't make keywords
+	 * TOCHANGE: use lemmatizer algorithm instead of simple stemmer
+	 * @param string
+	 * @return string
+	 */
+	private function simple_stemmer($string){
 	    $string = strtolower($string);
 	    //remove symbols and words less than 2 characters
 	    $string = trim( preg_replace("/[^a-z0-9']+([a-z0-9']{1,2}[^a-z0-9']+)*/i", " ", $string ) );
 	    return $string;
 	}
 
-	//Remove blacklisted words from string
-	//excluding words but, the, for, not, from, was, you, will, was, will, were etc
-	//But not buy, try, fly, see, eat, sit, run, sex, bed, bet etc
-	public function remove_blacklisted_words($string){
+	/*
+	 * Remove blacklisted words from string
+	 * excluding words but, the, for, not, from, you, will, was, were etc
+	 * But not buy, try, fly, see, eat, sit, run, sex, bed, bet etc
+	 * @param string
+	 * @return string
+	 */
+	private function remove_blacklisted_words($string){
 		//TOCHANGE: read blacklisted words from database instead defining array here
 	    $blackwords = array("'s","n't","the","all","you","for","from","between","and","but","not","was","will","were","are","who","which","why","how","when","this","that","those","these","their","has","had","more","most","very","much","can","its","with","into","been","only","need");
 	    //TOCHANGE: find method to replace all array at once without foreach loop
@@ -52,14 +61,22 @@ class Tfidf_cosine_model extends CI_Model{
 	    return $string;
 	}
 
-	//Count word frequency in given string
-	//returns array of $word => $count
-	public function count_word_frequency_in_string($text){
+	/*
+	 * Count word frequency in given string
+	 * returns array of $word => $count
+	 * @param string
+	 * @return array word as word => frequency
+	 */
+	private function count_word_frequency_in_string($text){
 	    return array_count_values(str_word_count($text,1));
 	}
 
-	//text to stemmed words array
-	public function text_to_stemmed_words_frequency($text){
+	/*
+	 * text to stemmed words array
+	 * @param string
+	 * @return array word as $word => frequency
+	 */
+	private function text_to_stemmed_words_frequency($text){
 		//clean the text
 		$text = $this->simple_stemmer($text);
 		$text = $this->remove_blacklisted_words($text);
@@ -70,11 +87,14 @@ class Tfidf_cosine_model extends CI_Model{
 		return $words;
 	}
 
-	//////////////////////////////////////// functions to insert words to database
+//////////////////////////// functions to insert words to database
 
-	//Function to clean text and insert words in it to allwords table
-	//calculate word count, then insert/update individual word with count
-	public function insert_words_allwords_table($words,$doc_id){
+	/*
+	 * Function to clean text and insert words in it to allwords table
+	 * calculate word count, then insert/update individual word with count
+	 * @param array words, int doc_id
+	 */
+	private function insert_words_allwords_table($words,$doc_id){
 		//insert each word with count
 		foreach($words as $word => $cnt){
 			//var_dump($cnt);die();
@@ -82,23 +102,29 @@ class Tfidf_cosine_model extends CI_Model{
 		}
 	}
 
-	//Save generated keyword to databse
-	public function save_keywords($keywords,$doc_id){
+	/*
+	 * Save generated keyword to databse
+	 * @param array $keywords, int doc_id
+	 */
+	private function save_keywords($keywords,$doc_id){
 		foreach($keywords as $key=>$value){
 			$this->db->query("UPDATE allwords set keyword=1 where doc_id=$doc_id and word='$value'");
 		}
 	}
 
-	/////////////////////////////////////// tfidf and keyword generator functions 
+///////////////////////////// tfidf and keyword generator functions 
 
-	//find Documnet frequency df to use in tfidf in tf * log( alldoc_count / df , 2 )
-	public function get_df($word){
+	/*
+	 * find Documnet frequency df to use in tfidf in tf * log( alldoc_count / df , 2 )
+	 * @param string word
+	 * @return int
+	 */
+	private function get_df($word){
 		$this->db->select('count(*) as df');
 		$this->db->from('allwords');
 		$this->db->where('word',$word);
 		$query = $this->db->get()->row();
 		return (int)$query->df;
-		
 	}
 
 	/*
@@ -107,7 +133,7 @@ class Tfidf_cosine_model extends CI_Model{
 	 * @param string word, int $doc_id
 	 * @return int tf
 	 */
-	public function get_tf($word,$doc_id){
+	private function get_tf($word,$doc_id){
 		$this->db->select('count as tf');
 		$this->db->from('allwords');
 		$this->db->where('word',$word);
@@ -116,23 +142,25 @@ class Tfidf_cosine_model extends CI_Model{
 		return (int)$query->tf;
 	}
 
-	//Find TF-IDF of individual given word tf * log( alldoc_count / df , 2 )
-	//takes input individual word and returns number
-	public function calculate_tfidf($word,$tf){
+	/*
+	 * Find TF-IDF of individual given word tf * log( alldoc_count / df , 2 )
+	 * takes input individual word and returns number
+	 * @param string word, int tf
+	 * @return float tfidf
+	*/
+	private function calculate_tfidf($word,$tf){
 		//var_dump($tf);die();
 		$tfidf = $tf * log ($this->alldoc_count / $this->get_df($word), 2);
 		return $tfidf;
 	}
 
-	//find tfidf for array of words and sort to find keywords
-	//we don't need to calculate tfidf for every word in words.
-	//As $words is already sorted, calculate tfidf of say 
-	//25% to 50% of the words for efficiency
-	//the lower frequency word generally does not make keyword
-	//but very rare word might make tfidf with frequency in about 75th percentile
-	//search Google for detailed TFIDF statistics before applying percentile filter
-	//returns sorted array of $word => $tfidf
-	public function generate_tfidf_array($words){
+	/*
+	 * find tfidf for array of words and sort to find keywords
+	 * returns sorted array of $word => $tfidf
+	 * @param array words as word => count
+	 * @return array words as word => tfidf
+	 */
+	private function generate_tfidf_array($words){
 		foreach($words as $key => $value){
 			//var_dump($key);var_dump($value);die();
 			$words[$key] = $this->calculate_tfidf($key,(int)$value);
@@ -144,45 +172,32 @@ class Tfidf_cosine_model extends CI_Model{
 		return $words;
 	}
 
-	//empty keywords, docs_keywords_link, allwords and allwords table
-	//So that we can store new values
-	//TOCHANGE: This may not necessary if we update count table on each insert
-	public function empty_tfidf_tables(){
+	/*
+	 * empty keywords, docs_keywords_link, allwords and allwords table
+	 * So that we can store new values
+	 */
+	private function empty_tfidf_tables(){
 		$this->db->query("truncate allwords");
-		//$this->db->query("truncate keywords");
-		//$this->db->query("truncate docs_keywords_link");
 	}
 
-	//Get fulltext for tfidf function.
-	//text for which tfidf is to be generated
-	// this function should return $doc_id => $text
-	public function get_all_text(){
-		//for diglib database
-		/*$this->db->select('doc_id, text');
-		$this->db->from('diglib_comments');
-		$query = $this->db->get();*/
-		
-		//for shopping cart database
+	/* 
+	 * Get fulltext for tfidf function.
+	 * text for which tfidf is to be generated
+	 * @return db object $doc_id => $text
+	 */
+	private function get_all_text(){		
 		$this->db->select('id as doc_id, description as text', FALSE);
 		$this->db->from('products');
 		$query = $this->db->get();
-
 		return $query->result();
 	}
-	///////////////////////////Following functions called from controller
-	///////////////////////////regenerate allwords and all keywords when we import documnets
 
-	//read all documents from alldocs table
-	public function get_alldocs_from_allwords(){
-		$this->db->select('*');
-		$this->db->from('allwords');
-		$query = $this->db->get();
-		return $query;
-	}
+//////////////////regenerate allwords and all keywords when we import documnets
 
-	//Regenerate tfidf database controller
-	//not necessary if we update allwords and keyword table each time we insert doc
-	//necessary if we import database
+	/*
+	 * not necessary if we update allwords table each time we insert doc
+	 * necessary if we import database
+	 */
 	public function regenerate_allwords_table(){
 		//first remove all previous data
 		$this->empty_tfidf_tables();
@@ -196,18 +211,22 @@ class Tfidf_cosine_model extends CI_Model{
 		}
 	}
 
-	//Regenerate keywords from regenerated allwords table
-	//Process keywords for all documents in database
-	//TOCHANGE: get sorted words from database
-
-	//to get all doc_id from allwords table
+	/*
+	 * to get all doc_id from allwords table
+	 * @return db object doc_id
+	 */
 	private function get_all_doc_id(){
 		$this->db->select('id');
 		$this->db->from('products');
 		$query = $this->db->get();
 		return $query->result();
 	}
-	//get all words for given doc_id
+
+	/*
+	 * get all words for given doc_id
+	 * @param int doc_id
+	 * @return db object words
+	 */
 	private function get_all_words_for_doc_id($doc_id){
 		$this->db->select('word,count');
 		$this->db->from('allwords');
@@ -217,7 +236,9 @@ class Tfidf_cosine_model extends CI_Model{
 		return $query->result();
 	}
 
-	//alternate regenerate_keywords_table()
+	/*
+	 * regenerate all keywords
+	 */
 	public function regenerate_keywords_table(){
 		$alldocs = $this->get_all_doc_id();
 		foreach ($alldocs as $id => $data) {
@@ -230,17 +251,14 @@ class Tfidf_cosine_model extends CI_Model{
 			$keywords = $this->tfidf_keywords($words,$doc_id);
 			$this->save_keywords($keywords,$doc_id);
 		}
-
 	}
 
-	/////////////end alternate methods
-
-///////////////////////////////////////////////
-
-	//INCOMPLETE
-	//to process keywords for individual docs
-	//updates allwords table and keyword table for individual doc
-	//use this function while saving the doc
+	/*
+	 * to process keywords for individual docs
+	 * updates allwords table and keyword table for individual doc
+	 * use this function while saving the doc
+	 * @param string
+	 */
 	public function process_text_keywords($text){
 		//generate stemmed words array with frequency from text and insert to db
 		$words = $this->text_to_stemmed_words_frequency($text);
@@ -249,5 +267,4 @@ class Tfidf_cosine_model extends CI_Model{
 		$keywords = $this->tfidf_keywords($words,$doc_id);
 		$this->save_keywords($keywords,$doc_id);
 	}
-
 }
